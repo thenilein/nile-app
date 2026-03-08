8a19956f8be7ae38e83fe29dac9c884bf5770e51
Praveen9789 <praveenkumar26833@gmail.com>
2026-03-08 21:48:58 +0530

    chore: Set Vite build base path to relative and update asset links.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
     return {
+        base: './',
         plugins: [react()],
         build: {
             sourcemap: mode === 'development' || mode === 'test',
